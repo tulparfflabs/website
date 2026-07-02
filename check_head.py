783cd273d18a20a774121a22823b4e28453d1546
aylinbahsi <bahsiaylin@gmail.com>
2026-07-02 23:30:54 +0300

    new

--- check_head.py
+++
@@ -1,8 +0,0 @@
-﻿import re
-
-with open('live_test_mobile.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-
-head = re.search(r'<head>(.*?)</head>', html, re.DOTALL)
-if head:
-    print(head.group(1).strip()[:1000])
--- take_screenshot.js
+++
@@ -1,9 +0,0 @@
-﻿const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 390, height: 844, isMobile: true });
-  await page.goto('http://localhost:5000/index.html');
-  await page.screenshot({ path: 'local_screenshot.png' });
-  await browser.close();
-})();
